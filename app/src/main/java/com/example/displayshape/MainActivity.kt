42deabcb969042a3eb0be5b18c8838588fedc69f
นางสาวมุนินทร์ วุฒิพงศ์วรกิจ <6310682601@eng.tu>
2023-03-17 10:59:01 +0700

    create app

---
+++ app/src/main/java/com/example/displayshape/MainActivity.kt
@@ -0,0 +1,101 @@
+package com.example.displayshape
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import com.example.displayshape.ui.theme.DisplayshapeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            DisplayshapeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    MyScreen()
+                }
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun MyScreenPreview() {
+    MyScreen()
+}
+@Composable
+fun MyScreen() {
+    var shape by remember { mutableStateOf(RectangleShape) }
+    Column {
+        Row(horizontalArrangement = Arrangement.SpaceAround) {
+            Button(onClick = { shape = RectangleShape }) { Text("Square") }
+            Button(onClick = { shape = CircleShape }) { Text("Circle") }
+            Button(onClick = { shape = RoundedCornerShape(10.dp) }) { Text("RoundedCornerShape") }
+        }
+        ExampleBox(shape)
+    }
+}
+
+@Preview
+@Composable
+fun RectangleShapeDemo(){
+    ExampleBox(shape = RectangleShape)
+}
+
+@Preview
+@Composable
+fun CircleShapedemo() {
+    ExampleBox(shape = CircleShape)
+}
+
+@Preview
+@Composable
+fun RoundedCornerShapeDemo(){
+    ExampleBox(shape = RoundedCornerShape(10.dp))
+}
+
+@Composable
+fun ExampleBox(shape: Shape){
+    Column(modifier = Modifier
+        .fillMaxWidth()
+        .wrapContentSize(Alignment.Center)) {
+        Box(
+            modifier = Modifier
+                .size(100.dp)
+                .clip(shape)
+                .background(Color.Red)
+        )
+    }
+}
